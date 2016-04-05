26fed84a73a987c97d37bc05a3d53a20e8d72c63
WinnieDave <fnc01@mail.ru>
2016-04-05 21:53:34 +0300

    прост коммит

--- код-запара для тупого Яши.py
+++ код-запара для тупого Яши.py
@@ -8,25 +8,27 @@ if c > a:
     print('IT IS IMPOSSIBLE!')  # cюда вставить функцию, которая обрывала бы график
 if d > b:
     print('IT IS IMPOSSIBLE!')
-if co > c:
-    if do > d:
-        if c > d:
-            print(d, 'METERS AT MINIMAL')
-        else:
-            print(c, 'METERS AT MINIMAL')
-    else:
-        if c > do:
-            print(do, 'METERS AT MINIMAL')
-        else:
-            print(c, 'METERS AT MINIMAL')
-else:
-    if do > d:
-        if co > d:
-            print(d, 'METERS AT MINIMAL')
-        else:
-            print(co, 'METERS AT MINIMAL')
-    else:
-        if co > do:
-            print(do, 'METERS AT MINIMAL')
-        else:
-            print(co, 'METERS AT MINIMAL')
+array=[c,d,co,do];#об`явление массива
+print(min(array));#вывод наименьшего.Ото й усе.
+# if co > c:
+#     if do > d:
+#         if c > d:
+#             print(d, 'METERS AT MINIMAL')
+#         else:
+#             print(c, 'METERS AT MINIMAL')
+#     else:
+#         if c > do:
+#             print(do, 'METERS AT MINIMAL')
+#         else:
+#             print(c, 'METERS AT MINIMAL')
+# else:
+#     if do > d:
+#         if co > d:
+#             print(d, 'METERS AT MINIMAL')
+#         else:
+#             print(co, 'METERS AT MINIMAL')
+#     else:
+#         if co > do:
+#             print(do, 'METERS AT MINIMAL')
+#         else:
+#             print(co, 'METERS AT MINIMAL')
