2b58ea3a238e9fcd4c136715c2f676fcf2ad8124
kleineG1zmo <koliostroba@gmail.com>
2016-04-08 22:35:07 +0300

    Delete calender.py

--- calender.py
+++
@@ -1,9 +0,0 @@
-a = int(input('PRINT YEAR HERE '))
-if a % 4 == 0 and a % 400 == 0:
-    print ('YES')
-elif a % 4 == 0 and a % 100 == 0:
-    print ('NO')
-elif a % 4 == 0:
-    print ('YES')
-else:
-    print ('NO')
