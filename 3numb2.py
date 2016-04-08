864a13fc608d431b34afe3f64954dd2d32eeac07
kleineG1zmo <koliostroba@gmail.com>
2016-04-08 22:34:39 +0300

    Delete 3numb2.py

--- 3numb2.py
+++
@@ -1,9 +0,0 @@
-a = int(input('NUMBER 1 HERE '))
-b = int(input('NUMBER 2 HERE '))
-c = int(input('NUMBER 3 HERE '))
-if a == b == c:
-    print ('3')
-elif (a == b != c) or (a != b == c) or (a == c != b):
-    print ('2')
-else:
-    print ('0')
