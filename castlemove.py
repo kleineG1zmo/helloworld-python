900f252d20d54eebb8f32df243f45b1686a91fe6
kleineG1zmo <koliostroba@gmail.com>
2016-04-08 22:35:01 +0300

    Delete castlemove.py

--- castlemove.py
+++
@@ -1,8 +0,0 @@
-a = int(input('CROSS 1 X '))
-b = int(input('CROSS 1 Y '))
-c = int(input('CROSS 2 X '))
-d = int(input('CROSS 2 Y '))
-if a == c or b == d:
-    print ('YES')
-else:
-    print ('NO')
