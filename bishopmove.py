b8a70eeadde8c8591b10e03cbef35dda41d7ca82
kleineG1zmo <koliostroba@gmail.com>
2016-04-08 22:34:55 +0300

    Delete bishopmove.py

--- bishopmove.py
+++
@@ -1,8 +0,0 @@
-a = int(input('CROSS 1 X '))
-b = int(input('CROSS 1 Y '))
-c = int(input('CROSS 2 X '))
-d = int(input('CROSS 2 Y '))
-if (a - c) == (d - b) or (a - c) == -(d - b) or -(a - c) == (d - b):
-    print ('YES')
-else:
-    print ('NO')
