365c53c4a59064505d285a694c47c91d93934fa9
Dmytro Vasyk <Dimonous@users.noreply.github.com>
2016-04-11 09:38:54 +0300

    Run test

--- Lesson 3/nolib^_^.py
+++ Lesson 3/nolib^_^.py
@@ -1,2 +1,6 @@
 a = float(input('YOUR NUMBER '))
 print(a - int(a)) # WHAT A FUCK? DAVED, TEST IT, `COS IT WORKS UNCORRECTLY AND TELL ME ABOUT THE ERROR IN TELEGRAM!
+# I don't know whats wrong? Works right on my Linux : 
+# web@dev:~$ python nolib^_^.py
+# YOUR NUMBER 12.8
+# 0.8
